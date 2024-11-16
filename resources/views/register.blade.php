591a195188b4e2f0480d72d07bffa57ee0639de4
rizaldiabyannata <aldizar255@gmail.com>
2024-11-16 14:17:28 +0800

    update style

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@400;700&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
     <title>Register</title>
 </head>
-<body>
+
+<body class="font-['Karantina']">
     <div class="flex items-center justify-center min-h-screen "> <!--border-l-2 border-r-2-->
-    <x-navbar></x-navbar>
+        <x-navbar></x-navbar>
         <div class="flex justify-center pt-4 border-black">
             <div class="bg-white border border-black shadow-md px-8 py-6 max-w-md w-[710px] h-[470px]">
                 <div class="flex justify-center">
-                    <p class="font-karantina font-semibold text-4xl">REGISTER</p>
+                    <p class="font-semibold text-4xl">REGISTER</p>
                 </div>
                 <div class="mt-3">
-                    <p class="font-karantina font-semibold text-2xl">USERNAME</p>
+                    <p class="font-semibold text-2xl">USERNAME</p>
                     <div>
                         <form action="">
                             <input type="text" placeholder="" class="mt-2 p-3 border border-gray-300  w-full">
@@ -24,7 +25,7 @@
                     </div>
                 </div>
                 <div class="mt-3">
-                    <p class="font-karantina font-semibold text-2xl">EMAIL</p>
+                    <p class="font-semibold text-2xl">EMAIL</p>
                     <div>
                         <form action="">
                             <input type="text" placeholder="" class="mt-2 p-3 border border-gray-300  w-full">
@@ -32,7 +33,7 @@
                     </div>
                 </div>
                 <div class="mt-3">
-                    <p class="font-karantina font-semibold text-2xl">PASSWORD</p>
+                    <p class="font-semibold text-2xl">PASSWORD</p>
                     <div>
                         <form action="">
                             <input type="text" placeholder="" class="mt-2 p-3 border border-gray-300  w-full">
@@ -40,11 +41,12 @@
                     </div>
                 </div>
                 <div class="mt-4 flex">
-                    <button class="bg-[#00599D] text-white font-karantina font-semibold text-4xl w-full p-3 border-black shadow-sm">REGISTER</button>
+                    <button class="bg-[#00599D] text-white font-semibold text-4xl w-full p-3 border-black shadow-sm">REGISTER</button>
                 </div>
             </div>
         </div>
     </div>
     <x-footer></x-footer>
 </body>
+
 </html>
