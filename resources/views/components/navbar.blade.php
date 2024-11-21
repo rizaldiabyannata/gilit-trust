b01db159dea87c510d6065d9f47b2971c0dad776
Hazim <hazimmaulana2@gmail.com>
2024-11-22 01:49:57 +0800

    footnav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,16 @@
-<nav class="font-black flex flex-row justify-between items-center px-10 py-10 bg-slate-300 text-xl w-full fixed top-0">
-    <a href=""><img src="" alt="Logo"></a>
-    <div class="flex flex-row justify-center items-center space-x-6">
-        <a href="">Trip's</a><a href="">Review</a><a href="">About</a>
-    </div>
-    <div class="flex flex-row justify-center items-center space-x-6">
-        <a href="">Login</a>
-        <a href="">Register</a>
+<nav class="font-semibold text-lg flex flex-row items-center px-10 h-20 w-full fixed top-0 shadow-md font-poppins">
+    <a href="">
+        <img src="./picture/logo.png" class="w-[95px] h-[50px]" alt="Logo">
+    </a>
+    <div class="flex w-full justify-between">
+        <div class="flex flex-row justify-start items-center space-x-6 pl-10">
+            <a href="">Blog</a>
+            <a href="">Galery</a>
+            <!-- <a href="">About</a> -->
+        </div>
+        <div class="flex flex-row justify-end items-center space-x-6">
+            <a href="">Login</a>
+            <a href="">Register</a>
+        </div>
     </div>
 </nav>
