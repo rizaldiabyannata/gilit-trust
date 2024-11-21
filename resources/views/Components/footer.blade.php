dc4706552fd76ee0561d5945d2c9939b9bde5b6c
rizaldiabyannata <aldizar255@gmail.com>
2024-11-22 02:49:42 +0800

    delete error file

--- resources/views/Components/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="font-['Karantina'] flex flex-row justify-center items-center h-24 bg-slate-300 text-3xl w-full fixed bottom-0">
-    <p>FOOTER</p>
-</footer>
--- resources/views/components/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="font-poppins flex flex-row justify-center items-center h-20 w-full fixed bottom-0 [box-shadow:0_-0.2rem_0.2rem_rgba(0,0,0,0.1)]">
-    <p>FOOTER</p>
-</footer>
