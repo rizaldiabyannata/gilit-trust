9b7a8c7a53c4f1eb05bb242e60d000895618226f
Hazim <hazimmaulana2@gmail.com>
2024-11-24 02:46:18 +0800

    feat-blog

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+
+<body class="flex flex-col w-full font-poppins">
+    <x-navbarMain></x-navbarMain>
+    <div class="flex flex-col px-36 pt-[100px]">
+        <a href="" class="flex flex-row w-full  space-x-12  h-[260px] items-center">
+            <div class="flex flex-col">
+                <div class="flex flex-row">
+                    <img src="{{ asset('picture/logo.png') }}" class="w-[90px] h-[50px]" alt="">
+                    <div class="flex items-center pl-4">Gili Trust By Indra</div>
+                </div>
+                <div class="pt-6 text-3xl font-bold">Mengenal Lebih Dalam Keindahan dari Gili MATRA</div>
+                <div class="text-justify pt-2">
+                menjelajahi pesona alam dan budaya tiga pulau cantik di Lombok Utara: Gili Trawangan, Gili Meno, dan Gili Air. Blog ini membahas keunikan masing-masing pulau, mulai dari keindahan bawah laut, pantai berpasir putih, hingga aktivitas seru seperti snorkeling, diving, dan menyaksikan matahari terbenam.
+                </div>
+                <div class="flex flex-row pt-2 items-center">
+                    <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 0.25C13.9 0.25 12.05 1.3 11 2.95C9.95 1.3 8.1 0.25 6 0.25C2.7 0.25 0 2.95 0 6.25C0 12.2 11 18.25 11 18.25C11 18.25 22 12.25 22 6.25C22 2.95 19.3 0.25 16 0.25Z" fill="#F44336"/>
+                    </svg>
+                    <div class="pl-2">23 Likes</div>
+                </div>
+            </div>
+            <img src="{{ asset('picture/blog.png') }}" class="" alt="">
+        </a>
+        <div class="border-t mx-6 border-[#C4CDD5]"></div>
+        <a href="" class="flex flex-row w-full  space-x-12  h-[260px] items-center">
+            <div class="flex flex-col">
+                <div class="flex flex-row">
+                    <img src="{{ asset('picture/logo.png') }}" class="w-[90px] h-[50px]" alt="">
+                    <div class="flex items-center pl-4">Gili Trust By Indra</div>
+                </div>
+                <div class="pt-6 text-3xl font-bold">Mengenal Lebih Dalam Keindahan dari Gili MATRA</div>
+                <div class="text-justify pt-2">
+                menjelajahi pesona alam dan budaya tiga pulau cantik di Lombok Utara: Gili Trawangan, Gili Meno, dan Gili Air. Blog ini membahas keunikan masing-masing pulau, mulai dari keindahan bawah laut, pantai berpasir putih, hingga aktivitas seru seperti snorkeling, diving, dan menyaksikan matahari terbenam.
+                </div>
+                <div class="flex flex-row pt-2 items-center">
+                    <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 0.25C13.9 0.25 12.05 1.3 11 2.95C9.95 1.3 8.1 0.25 6 0.25C2.7 0.25 0 2.95 0 6.25C0 12.2 11 18.25 11 18.25C11 18.25 22 12.25 22 6.25C22 2.95 19.3 0.25 16 0.25Z" fill="#F44336"/>
+                    </svg>
+                    <div class="pl-2">23 Likes</div>
+                </div>
+            </div>
+            <img src="{{ asset('picture/blog.png') }}" class="" alt="">
+        </a>
+        <div class="border-t mx-6 border-[#C4CDD5]"></div>
+        <a href="" class="flex flex-row w-full  space-x-12  h-[260px] items-center">
+            <div class="flex flex-col">
+                <div class="flex flex-row">
+                    <img src="{{ asset('picture/logo.png') }}" class="w-[90px] h-[50px]" alt="">
+                    <div class="flex items-center pl-4">Gili Trust By Indra</div>
+                </div>
+                <div class="pt-6 text-3xl font-bold">Mengenal Lebih Dalam Keindahan dari Gili MATRA</div>
+                <div class="text-justify pt-2">
+                menjelajahi pesona alam dan budaya tiga pulau cantik di Lombok Utara: Gili Trawangan, Gili Meno, dan Gili Air. Blog ini membahas keunikan masing-masing pulau, mulai dari keindahan bawah laut, pantai berpasir putih, hingga aktivitas seru seperti snorkeling, diving, dan menyaksikan matahari terbenam.
+                </div>
+                <div class="flex flex-row pt-2 items-center">
+                    <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 0.25C13.9 0.25 12.05 1.3 11 2.95C9.95 1.3 8.1 0.25 6 0.25C2.7 0.25 0 2.95 0 6.25C0 12.2 11 18.25 11 18.25C11 18.25 22 12.25 22 6.25C22 2.95 19.3 0.25 16 0.25Z" fill="#F44336"/>
+                    </svg>
+                    <div class="pl-2">23 Likes</div>
+                </div>
+            </div>
+            <img src="{{ asset('picture/blog.png') }}" class="" alt="">
+        </a>
+        <div class="border-t mx-6 border-[#C4CDD5]"></div>
+        <div class="flex items-center justify-center space-x-1 pt-6">
+                <button class="w-[40px] h-[40px] bg-[#C4CDD5] rounded-lg text-white flex justify-center items-center">
+                    <svg width="9" height="12" viewBox="0 0 9 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.16016 1.41L3.58016 6L8.16016 10.59L6.75016 12L0.750156 6L6.75016 0L8.16016 1.41Z" fill="white"/>
+                    </svg>
+                </button>
+                <button class="w-[40px] h-[40px] border border-[#C4CDD5] rounded-lg text-black">1</button>
+                <button class="w-[40px] h-[40px] border border-[#C4CDD5] rounded-lg text-black">2</button>
+                <button class="w-[40px] h-[40px] border border-[#C4CDD5] rounded-lg text-black">3</button>
+                <button class="w-[40px] h-[40px] border border-[#C4CDD5] rounded-lg text-black">4</button>
+                <button class="w-[40px] h-[40px] border border-[#C4CDD5] rounded-lg text-[#C4CDD5] flex justify-center items-center">
+                    <svg width="9" height="12" viewBox="0 0 9 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.839844 1.41L5.41984 6L0.839844 10.59L2.24984 12L8.24984 6L2.24984 0L0.839844 1.41Z" fill="#C4CDD5"/>
+                    </svg>
+                </button>
+            </div>
+    </div>
+
+    <x-footerMain></x-footerMain>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\MainPageController;
 use App\Http\Controllers\TripController;
+use App\Http\Controllers\BlogController;
 
 Route::get("/login", [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'auth']);
@@ -16,3 +17,5 @@ Route::Post("/register", [registerController::class, 'auth']);
 Route::get("/home", [MainPageController::class, 'index']);
 
 Route::get("/trip", [TripController::class, 'index']);
+
+Route::get("/blog", [BlogController::class, 'index']);
