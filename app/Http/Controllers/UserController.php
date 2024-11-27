55c9ad8e68528abbf9c8845f7c5df876f324713f
rizaldiabyannata <aldizar255@gmail.com>
2024-11-27 21:31:33 +0800

    backend: user CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|confirmed',
+            'password_confirmation' => 'required|string'
+        ]);
+
+        $user = User::create($request->all());
+        return redirect()->route('users.index')->with('success', 'User created successfully!');
+    }
+
+
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('users.show', compact('user'));
+    }
+
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,' . $id,
+            'password' => 'required|string|confirmed',
+            'password_confirmation' => 'required|string'
+        ]);
+
+        $user = User::find($id);
+        $user->update($request->all());
+        return redirect()->route('users.index')->with('success', 'User updated successfully!');
+    }
+
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User deleted successfully!');
+    }
 }
